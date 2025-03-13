da104eab1502fd2b80a732099d55d9491b33498d
Wary <evenfbh@gmail.com>
2025-03-13 12:51:38 +0100

    Jobbet vidre på tårn

--- towerattack.gd
+++ towerattack.gd
@@ -4,8 +4,9 @@ extends Sprite2D
 var attackSpeed = 100
 var dir : float
 var spawnPos : Vector2
-
 var enemy = preload("res://Enemies/enemy_test.tscn")
+
 func _process(delta: float) -> void:	
 	global_position = spawnPos
+	
 	
